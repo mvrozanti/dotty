d67959ce37c19c127af5bb883c05f05316628489
Marcelo <mvrozanti@hotmail.com>
2018-09-08 21:08:42 -0200

    clear works better now - removing everything but .git and dotty from dotfile directory

--- dotty/dotty.py
+++ dotty/dotty.py
@@ -23,14 +23,12 @@ import sys
 import argparse
 import errno
 import code
-import os.path as op
 
 # Fix Python 2.x
 try: input = raw_input
 except NameError: pass
 
-chdir_dotfiles = lambda config: os.chdir(
-            op.join(op.expanduser(op.dirname(config)), os.pardir))
+chdir_dotfiles = lambda config: os.chdir(os.path.dirname(os.path.expanduser(os.path.abspath(os.path.dirname(config)))))
 prompt_user = True
 dry_run = False
 
@@ -51,21 +49,21 @@ def ask_user(prompt):
         else: print("Enter a correct choice.", file=sys.stderr)
 
 def create_directory(path):
-    exp = op.expanduser(path)
-    if (not op.isdir(exp)):
+    exp = os.path.expanduser(path)
+    if (not os.path.isdir(exp)):
         print("{0} doesnt exist, creating.".format(exp))
         os.makedirs(exp)
 
 def create_symlink(src, dst):
-    dst = op.expanduser(dst)
-    src = op.abspath(src)
-    broken_symlink = op.lexists(dst) and not op.exists(dst)
-    if op.lexists(dst):
-        if op.islink(dst) and os.readlink(dst) == src:
+    dst = os.path.expanduser(dst)
+    src = os.path.abspath(src)
+    broken_symlink = os.path.lexists(dst) and not os.path.exists(dst)
+    if os.path.lexists(dst):
+        if os.path.islink(dst) and os.readlink(dst) == src:
             print("Skipping existing {0} -> {1}".format(dst, src))
             return
         elif prompt_user or ask_user("{0} exists, delete it? [Y/a/n]".format(dst)):
-            if op.isfile(dst) or broken_symlink or op.islink(dst): os.remove(dst)
+            if os.path.isfile(dst) or broken_symlink or os.path.islink(dst): os.remove(dst)
             else: shutil.rmtree(dst)
         else: return
     print("Linking {0} -> {1}".format(dst, src))
@@ -75,43 +73,43 @@ def create_symlink(src, dst):
         symlink = ctypes.windll.kernel32.CreateSymbolicLinkW
         symlink.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
         symlink.restype = ctypes.c_ubyte
-        flags = 1 if op.isdir(src) else 0
+        flags = 1 if os.path.isdir(src) else 0
         symlink(dst, src, flags)
 
 def copy_path(src, dst, backup=False):
-    dst = op.expanduser(dst) if not backup else op.abspath(dst)
-    src = op.abspath(src) if not backup else op.expanduser(src)
-    if op.exists(dst):
+    dst = os.path.expanduser(dst) if not backup else os.path.abspath(dst)
+    src = os.path.abspath(src) if not backup else os.path.expanduser(src)
+    if os.path.exists(dst):
         if not remove_path(dst): return 
     print("Copying {0} -> {1}".format(src, dst))
-    if op.isfile(src): 
+    if os.path.isfile(src): 
         try: shutil.copy(src, dst) 
         except Exception as e:
             if e.errno not in [errno.ENOENT, errno.ENXIO]: raise
-            os.makedirs(op.dirname(dst))
+            os.makedirs(os.path.dirname(dst))
             shutil.copy(src, dst) 
     else: 
         try: shutil.copytree(src, dst)
         except: pass
 
 def remove_path(path):
-    path = op.abspath(path)
+    path = os.path.abspath(path)
     if prompt_user or ask_user("{0} exists, delete it? [Y/a/n]".format(path)):
-        if op.isfile(path) or op.islink(path): os.remove(path)
+        if os.path.isfile(path) or os.path.islink(path): os.remove(path)
         else: shutil.rmtree(path)
         return True
     else: return False
 
 def move(src, dst):
-    dst = op.abspath(dst)
-    if op.exists(dst) and remove_path(dst): shutil.move(src,dst)
+    dst = os.path.abspath(dst)
+    if os.path.exists(dst) and remove_path(dst): shutil.move(src,dst)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", metavar='*dotty*.json',       help="the JSON file you want to use, \n\
             it's only required if filename doesn't end in json or doesn't contain dotty in the basename", required=False)
     parser.add_argument("-f", "--force",   action="store_true", help="\033[1mdoes not prompt user\033[0m: replace files/folders if they already exist, removing previous directory tree")
-    parser.add_argument("-b", "--backup",  action="store_true", help="run copy in reverse so that files and directories are backed up to the directory the config file is in", default=len(sys.argv) > 2)
+    parser.add_argument("-b", "--backup",  action="store_true", help="run copy in reverse so that files and directories are backed up to the directory the config file is in", default=True)
     parser.add_argument("-c", "--clear",   action="store_true", help="clears the config directory before anything, removing all files listed in it")
     parser.add_argument("-r", "--restore", action="store_true", help="restore all elements to system (mkdirs, link, copy, install(install_cmd), commands)")
     parser.add_argument("-d", "--dryrun",  action="store_true", help="perform a dry run, outputting what changes would have been made if this argument was removed [TODO]")
@@ -119,18 +117,17 @@ def main():
     args = parser.parse_args()
     prompt_user = not args.force
     if not args.config: # look in parent directory of this script
-        dir_path = op.dirname(op.realpath(__file__))
+        dir_path = os.path.dirname(os.path.realpath(__file__))
         for f in os.listdir(dir_path):
-            basename = op.basename(f)
+            basename = os.path.basename(f)
             if all(name in basename for name in ['dotty','json']): 
-               args.config = op.join(dir_path, f)
+               args.config = os.path.join(dir_path, f)
                print('Found dotty configuration')
     if args.config is None: raise Exception('JSON config file is missing, add it to this script\'s folder')
     js = json.load(open(args.config))
     chdir_dotfiles(args.config) 
     if args.clear or args.eject:
-        for f in os.listdir(op.join(op.dirname(args.config), os.pardir)):
-            if not any(name in op.basename(f) for name in ['dotty','.git']): remove_path(f)
+        [remove_path(src) for src, _ in js['copy'].items()]
     if args.eject:
         print('TO BE IMPLEMENTED')
         return 
