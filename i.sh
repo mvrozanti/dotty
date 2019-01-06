be0069389b3c4c3e253d9e0c6c54bbafcfc88767
Marcelo <mvrozanti@hotmail.com>
2019-01-06 01:53:25 -0200

    ish++

--- i.sh
+++ i.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DOTTY_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 REPO_DIR=$DOTTY_DIR/..
-cat $REPO_DIR/.dottyrc.json|jq -r '.copy|to_entries[]|[.key,.value]|@tsv'
+cat $REPO_DIR/.dottyrc.json|jq -r '.copy|to_entries[]|[.key,.value]|@tsv' | awk -F'\t' '{print "mv "$1" "$2}' | sed '/^$/d' | xargs -n3 -I{} maybe sh -c '{}'
