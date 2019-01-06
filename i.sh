bc376a28537af98ee692db880ed3e4b4f9631439
Marcelo <mvrozanti@hotmail.com>
2019-01-06 02:06:45 -0200

    ish++

--- i.sh
+++ i.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 DOTTY_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 REPO_DIR=$DOTTY_DIR/..
-cat $REPO_DIR/.dottyrc.json|jq -r '.copy|to_entries[]|[.key,.value]|@tsv' | awk -F'\t' '{print "mv "$1" "$2}' | sed '/^$/d' | xargs -n3 -I{} maybe sh -c '{}'
+RC=$REPO_DIR/.dottyrc.json
+#cat $RC|jq -r '.copy|to_entries[]|[.key,.value]|@tsv' | awk -F'\t' '{print "mv "$1" "$2}' | sed '/^$/d' | xargs -n3 -I{} maybe sh -c '{}'
+install_cmd=`cat $RC|jq -r '.install_cmd'`
+# install with install_cmd
+cat $RC|jq -r '.install|@tsv'|sed 's/\t/ /g'|xargs -L 1 -I{} sh -c $install_cmd' {}' #| awk -F'\t' '{print "mv "$1" "$2}' | sed '/^$/d' | xargs -n3 -I{} maybe sh -c '{}'
